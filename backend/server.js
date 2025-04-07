006a453df626e278220f62ab0a5b6cbd483115d6
Barry Weinstein <pillowcasestudies@gmail.com>
2025-04-07 11:56:22 -0400

    introducing a view

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-
+const path = require('path');
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors());
@@ -13,6 +13,8 @@ app.use(express.json());
 app.get("/health", (req, res) => {
   res.status(200).json({ status: "ok", message: "Server is running" });
 });
+
+app.use(express.static(path.join(__dirname, "public")));
 
 // Start the servers
 app.listen(PORT, () => {
