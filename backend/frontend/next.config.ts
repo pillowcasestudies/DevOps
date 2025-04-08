5bc912b5a1cbc4decaa537ca8d338dd18c1bac06
Barry Weinstein <pillowcasestudies@gmail.com>
2025-04-08 18:06:26 -0400

    static exports

--- backend/frontend/next.config.ts
+++ backend/frontend/next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  // other configuration options if needed
 };
 
 export default nextConfig;
