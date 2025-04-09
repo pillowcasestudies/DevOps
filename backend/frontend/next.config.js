92ba20421f33ec8e64da5c0097580c75aa90aae0
Barry Weinstein <pillowcasestudies@gmail.com>
2025-04-09 09:14:03 -0400

    built static files for front end deployment with kubernetes

---
+++ backend/frontend/next.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',  // Enable static export
+  distDir: 'build', // Optional: Specifies the directory for the export
+};
+
+module.exports = nextConfig;
